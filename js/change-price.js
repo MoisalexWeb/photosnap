56ace28a4736dc796f17ba26fb9b855ad424a20e
Moises Caraballo <108103995+MoisalexWeb@users.noreply.github.com>
2023-09-05 14:11:13 -0400

    Update data JSON file in change-price.js

--- js/change-price.js
+++ js/change-price.js
@@ -4,7 +4,7 @@ const pricingBtn = document.querySelector(".plan__toggle__button"),
 
 const changePrice = async (plan) => {
     try {
-        const res = await fetch("data.json")
+        const res = await fetch("https://github.com/MoisalexWeb/photosnap/data.json")
         const data = await res.json()
         prices.forEach((price, index) => {
             price.textContent = data[plan][price.dataset.price]
